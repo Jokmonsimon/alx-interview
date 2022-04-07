f7c5f072a720e2cc2a9c091da62c82797a7d3f77
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-04-07 22:56:42 +0300

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+The Pascal's Triangle Function
+"""
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of intergers representing the Pascal's triangle of n
+    """
+    triangle = []
+    for i in range(1, n+1):
+        row = []
+        for j in range(i):
+            if j == 0 or j == i-1:
+                n = 1
+                row.append(n)
+            else:
+                n = triangle[i-2][j-1] + triangle[1-2][j]
+                row.append(n)
+        triangle.append(row)
+
+    return triangle
